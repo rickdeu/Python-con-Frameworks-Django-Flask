49cefb5c0f799a4abd9d8bef77c7f8be138ee112
rickdeu <hangaloandre@gmail.com>
2022-09-29 22:22:36 +0100

    commit all exercicse based on python course

---
+++ Lection01/main.py
@@ -0,0 +1,7 @@
+# Variable declaration
+
+name = "Andre Hangalo"
+phone = 940171369
+mail = "hangaloandre@gmail.com"
+
+print(f"====Personal Information====\nName:\t{name} \nPhone:\t{phone} \nE-mail:\t{mail}")
---
+++ README.md
@@ -0,0 +1,1 @@
+# Python-con-Frameworks-Django-Flask
